52e1e67857e4cd0dd39d58ac2ca28952f6d1fe99
shalik ram sapkota <sapkotashalikram@gmail.com>
2018-04-18 22:33:29 +0300

    Update urls.py

--- quickIT/quickIT/urls.py
+++ quickIT/quickIT/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+ """ here they goes various urls for websites"""
 urlpatterns = [
+   
     url(r'^admin/', include(admin.site.urls)),
 ]
